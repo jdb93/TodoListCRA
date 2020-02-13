cabefe513899decc2e10e49b88156bbf8bf8a78f
alumnoFI <alumnoFI@facultades.ort.edu.uy>
2020-02-13 19:49:46 -0300

    comments agregados

--- src/App.js
+++ src/App.js
@@ -3,26 +3,35 @@ import './App.scss';
 import Tarea from './components/Tarea';
 
 class App extends Component {
-
+//No necesito poner constructor con props, porque es innecesario
 state = {
   nombreTarea: '',
   tareas: [],
 };
-
+//En lugar de recibir al evento e, lo destructuro para quedarme con e.target
 changeNombreTarea = ({ target: { value } }) => {
+  //this.setState es asincrono. Recibe un json y actualiza
+  //el valor de las variables que tiene como claves
   this.setState({ nombreTarea: value });
 }
 
 agregarTarea = () => {
+  //Recordar siempre desestructurar tanto al state como a las props
+  // al comienzo de los métodos
   const { nombreTarea, tareas} = this.state;
+  //entrará si nombre tarea no es undefined, false, null, 0, etc
+  
   if(nombreTarea){
     const nuevaTarea = {nombre: nombreTarea};
+    //setState puede recibir mas de una pareja clave-valor!
     this.setState({ nombreTarea: '', tareas: [...tareas, nuevaTarea]});
   }
 };
 
   render(){
     const { nombreTarea, tareas } = this.state;
+    //Recordar que sobre los input de tipo texto, debemos tener una variable
+    //en el estado y un onChange para cada uno de ellos.
     return (
       <div className="App">
        <header className="App-header">
@@ -34,14 +43,20 @@ agregarTarea = () => {
         type="text" 
         placeholder="Ingrese descripcion tarea" 
         value= { nombreTarea } 
-        onChange={ this.changeNombreTarea } />
+        onChange={ this.changeNombreTarea } 
+        />
         <input 
         className="agregarTarea-boton" 
         type="button" 
         value="Agregar tarea"
-        onClick = { this.agregarTarea } />
+        onClick = { this.agregarTarea } 
+        />
       </div>
       <div className="grupoTareas">
+        {/*Recordar agregar un key a cada elemento raiz del 
+        map. Tip: usar index! Si el elemento ya tiene un id, usar el propio
+        id del elemento
+        */}
         {
           tareas.map((tarea, index) => (
             <Tarea key={index} tarea={tarea}/>
--- src/components/Tarea.js
+++ src/components/Tarea.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+//Ya estoy recibiendo desestructuradas a las props! No necesito
+// tener una variable para las props.
 const Tarea = ({tarea: {nombre}}) => <p>{nombre}</p>
 
 export default Tarea;
